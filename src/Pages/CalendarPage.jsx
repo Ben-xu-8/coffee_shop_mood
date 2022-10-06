b47f00f9c141678f51ef992b1afb14f4349cd8dd
Ben <xuben1996@gmail.com>
2022-10-06 03:55:01 -0400

    calendar

--- src/Pages/CalendarPage.jsx
+++ src/Pages/CalendarPage.jsx
@@ -7,44 +7,54 @@ import ReactDatePicker from 'react-datepicker';
 
 const CalendarPage = (props) => {
   const events = [{}];
+  let formats = {
+    timeGutterFormat: 'HH:mm',
+  };
   const localizer = momentLocalizer(moment);
   const [newEvent, setNewEvent] = useState({ title: '', start: '', end: '' });
   const [allEvents, setAllEvents] = useState(events);
 
   const handleEvent = () => {
     setAllEvents([...allEvents, newEvent]);
-    console.log(allEvents);
   };
 
   return (
     <div className='calendar'>
-      <div className='title'>Calendar</div>
-      <div className='inputTime'>
-        <input
-          type='text'
-          placeholder='Event Name'
-          valuye={newEvent.title}
-          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
-        />
-        <ReactDatePicker
-          selectsStart
-          placeholderText='Start Date'
-          selected={newEvent.start}
-          onChange={(start) => setNewEvent({ ...newEvent, start })}
-        />
-        <ReactDatePicker
-          selectsEnd
-          placeholderText='End Date'
-          selected={newEvent.end}
-          onChange={(end) => setNewEvent({ ...newEvent, end })}
-        />
-        <button onClick={handleEvent}>Submit</button>
+      <div className='entry'>
+        <div className='title'>Calendar</div>
+        <div className='inputTime'>
+          <input
+            className='inputValue'
+            type='text'
+            placeholder='Event Name'
+            value={newEvent.title}
+            onChange={(e) =>
+              setNewEvent({ ...newEvent, title: e.target.value })
+            }
+          />
+          <ReactDatePicker
+            className='inputValue'
+            selectsStart
+            placeholderText='Start Date'
+            selected={newEvent.startDate}
+            onChange={(startDate) => setNewEvent({ ...newEvent, startDate })}
+          />
+          <ReactDatePicker
+            className='inputValue'
+            selectsEnd
+            placeholderText='End Date'
+            selected={newEvent.endDate}
+            onChange={(endDate) => setNewEvent({ ...newEvent, endDate })}
+          />
+          <button onClick={handleEvent}>Submit</button>
+        </div>
       </div>
       <Calendar
+        formats={formats}
         localizer={localizer}
         events={allEvents}
-        startAccessor='start'
-        endAccessor='end'
+        startAccessor='startDate'
+        endAccessor='endDate'
       />
     </div>
   );
